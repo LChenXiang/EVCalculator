10a8e76003e9953fa590da7d02c5825dbc089eb9
NgYuKang <ngyukang01@gmail.com>
2021-09-11 15:45:20 +0800

    Added calculation function that calculates charge cost throughout entire session

--- app/calculator.py
+++ app/calculator.py
@@ -16,7 +16,7 @@ class Calculator():
                               [350, 50]]
 
     # you may add more parameters if needed, you may modify the formula also.
-    def cost_calculation(self, initial_state: int, final_state: int, capacity:int,
+    def cost_calculation(self, initial_state: float, final_state: float, capacity:int,
                          is_peak:bool, is_holiday:bool, base_price:int):
         if is_peak:
             peak_modifier = 1
@@ -57,6 +57,8 @@ class Calculator():
         time_to_add = timedelta(hours=charge_time)
         return starting_date_time + time_to_add
 
+
+
     # to be acquired through API
     def get_sun_hour(self, sun_hour):
         pass
@@ -80,4 +82,53 @@ class Calculator():
     def calculate_solar_energy(self):
         pass
 
+    def total_cost_calculation(self, start_date: date, start_time: time, end_time: datetime,
+                               start_state:int, final_state:int, base_price: int, power:int, capacity: int):
+        total_holiday_peak = 0
+        total_holiday_nonPeak = 0
+        total_nonHoliday_peak = 0
+        total_nonHoliday_nonPeak = 0
+        
+        current_date_time = datetime.combine(start_date, start_time)
+
+        reachedEnd = False
+        while (not reachedEnd):
+            holiday_surcharge = self.is_holiday(current_date_time.date())
+            peak = self.is_peak(current_date_time.time())
+            added_time = timedelta(hours=1)
+            new_datetime = min(end_time, (current_date_time + added_time).replace(minute=0, second=0, microsecond=0))
+            difference_time_minutes = (new_datetime - current_date_time).total_seconds()/60
+
+            if holiday_surcharge:
+                if peak:
+                    total_holiday_peak += difference_time_minutes
+                else:
+                    total_holiday_nonPeak += difference_time_minutes
+            else:
+                if peak:
+                    total_nonHoliday_peak += difference_time_minutes
+                else:
+                    total_nonHoliday_nonPeak += difference_time_minutes
+
+            if new_datetime == end_time:
+                reachedEnd = True
+            current_date_time = new_datetime
+
+        # Calculate Final state of charge for each category
+        holiday_peak_fsoc = (((total_holiday_peak/60)*power/capacity) + (start_state/100)) * 100
+        holiday_nonpeak_fsoc = (((total_holiday_nonPeak/60)*power/capacity) + (start_state/100)) * 100
+        nonholiday_peak_fsoc = (((total_nonHoliday_peak/60)*power/capacity) + (start_state/100)) * 100
+        nonholiday_nonpeak_fsoc = (((total_nonHoliday_nonPeak/60)*power/capacity)
+                                   + (start_state/100)) * 100
+
+        cost_holiday_peak = self.cost_calculation(start_state, holiday_peak_fsoc, capacity,
+                                                  True, True, base_price)
+        cost_holiday_nonpeak = self.cost_calculation(start_state, holiday_nonpeak_fsoc, capacity,
+                                                     False, True, base_price)
+        cost_nonholiday_peak = self.cost_calculation(start_state, nonholiday_peak_fsoc, capacity,
+                                                     True, False, base_price)
+        cost_nonholiday_nonpeak = self.cost_calculation(start_state, nonholiday_nonpeak_fsoc,
+                                                        capacity, False, False, base_price)
+
+        return round((cost_holiday_peak + cost_holiday_nonpeak + cost_nonholiday_peak + cost_nonholiday_nonpeak), 2)
 
