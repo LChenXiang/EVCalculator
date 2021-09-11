52ebf31851da407b8400bf0537e48b05a75d010b
NgYuKang <ngyukang01@gmail.com>
2021-09-11 19:07:36 +0800

    Changed back expected output after finishing CI/CD test

--- test/calculator_solarapi_unittest.py
+++ test/calculator_solarapi_unittest.py
@@ -30,7 +30,7 @@ class TestSunHours(unittest.TestCase):
         input_date = date(2021, 8, 20)
         postcode = "3200"
         res = self.calculator.get_day_light_length(input_date, postcode)
-        expected_output = 0 #10.8334
+        expected_output = 10.8334
         self.assertAlmostEqual(res, expected_output, delta=0.01, msg=("Expected %s, got %s" %
                                                                       (expected_output, res)))
 
