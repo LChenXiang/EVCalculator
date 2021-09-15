adc0cba1c34dc0b39fcc936fd30e1e80b1fc6b22
NgYuKang <ngyukang01@gmail.com>
2021-09-15 15:27:20 +0800

    Deleted branch coverage case and changed it to path coverage for total_cost_calculation whitebox testing cases

--- test/requirement_1_whitebox_unittest.py
+++ test/requirement_1_whitebox_unittest.py
@@ -13,17 +13,36 @@ class WhiteBoxCostCalculator(unittest.TestCase):
         self.calculator = Calculator()
         self.postcode = "4000"
 
-    def test_white_box_branch_1(self):
-        """
-        We will test the branch of non-holiday weekend on both peak and non peak hours.
-        """
-        config = 6
-        start_time = time(5, 30)
+    def test_whitebox_totalcost_tc1(self):
+        config = 1
+        start_time = time(8)
+        start_date = date(2021, 8, 24)
+        battery_capacity = 50
+        initial_charge = 20
+        final_charge = 40
+        expected_cost = 0.55
+        power = self.calculator.get_configuration(config)[0]
+        base_cost = self.calculator.get_configuration(config)[1]
+        charge_time = self.calculator.time_calculation(initial_state=initial_charge, final_state=final_charge,
+                                                       capacity=battery_capacity, power=power)
+        end_time = self.calculator.get_end_time(start_date, start_time, charge_time)
+        final_cost = self.calculator.total_cost_calculation(start_date=start_date, start_time=start_time,
+                                                            start_state=initial_charge, end_time=end_time,
+                                                            base_price=base_cost, power=power,
+                                                            capacity=battery_capacity, postcode=self.postcode)
+
+        self.assertAlmostEqual(final_cost, expected_cost, delta=0.01, msg=("Expected %s, got %s instead"
+                                                                           % (expected_cost,final_cost)))
+
+
+    def test_whitebox_totalcost_tc2(self):
+        config = 1
+        start_time = time(0)
         start_date = date(2021, 8, 21)
         battery_capacity = 50
         initial_charge = 20
-        final_charge = 80
-        expected_cost = 4.2
+        final_charge = 40
+        expected_cost = 0.25
         power = self.calculator.get_configuration(config)[0]
         base_cost = self.calculator.get_configuration(config)[1]
         charge_time = self.calculator.time_calculation(initial_state=initial_charge, final_state=final_charge,
@@ -33,21 +52,37 @@ class WhiteBoxCostCalculator(unittest.TestCase):
                                                             start_state=initial_charge, end_time=end_time,
                                                             base_price=base_cost, power=power,
                                                             capacity=battery_capacity, postcode=self.postcode)
-
         self.assertAlmostEqual(final_cost, expected_cost, delta=0.01, msg=("Expected %s, got %s instead"
-                                                                           % (expected_cost, final_cost)))
+                                                                           % (expected_cost,final_cost)))
 
-    def test_white_box_branch_2(self):
-        """
-        We will test the branch of holidays/weekdays branch of peak and non-peak hours.
-        """
+    def test_whitebox_totalcost_tc3(self):
         config = 3
-        start_time = time(16, 50)
-        start_date = date(2021, 8, 20)
+        start_time = time(8)
+        start_date = date(2021, 8, 21)
         battery_capacity = 50
         initial_charge = 20
-        final_charge = 80
-        expected_cost = 2.11
+        final_charge = 40
+        expected_cost = 1
+        power = self.calculator.get_configuration(config)[0]
+        base_cost = self.calculator.get_configuration(config)[1]
+        charge_time = self.calculator.time_calculation(initial_state=initial_charge, final_state=final_charge,
+                                                       capacity=battery_capacity, power=power)
+        end_time = self.calculator.get_end_time(start_date, start_time, charge_time)
+        final_cost = self.calculator.total_cost_calculation(start_date=start_date, start_time=start_time,
+                                                            start_state=initial_charge, end_time=end_time,
+                                                            base_price=base_cost, power=power,
+                                                            capacity=battery_capacity, postcode=self.postcode)
+        self.assertAlmostEqual(final_cost, expected_cost, delta=0.01, msg=("Expected %s, got %s instead"
+                                                                           % (expected_cost,final_cost)))
+
+    def test_whitebox_totalcost_tc4(self):
+        config = 4
+        start_time = time(0)
+        start_date = date(2021, 8, 24)
+        battery_capacity = 50
+        initial_charge = 20
+        final_charge = 40
+        expected_cost = 0.69
         power = self.calculator.get_configuration(config)[0]
         base_cost = self.calculator.get_configuration(config)[1]
         charge_time = self.calculator.time_calculation(initial_state=initial_charge, final_state=final_charge,
@@ -59,7 +94,11 @@ class WhiteBoxCostCalculator(unittest.TestCase):
                                                             capacity=battery_capacity, postcode=self.postcode)
 
         self.assertAlmostEqual(final_cost, expected_cost, delta=0.01, msg=("Expected %s, got %s instead"
-                                                                           % (expected_cost, final_cost)))
+                                                                           % (expected_cost,final_cost)))
+
+
+
+
 
     def test_white_box_invalid_end_date(self):
         """
