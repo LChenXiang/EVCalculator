10e13e1371c88c36b4ed0ed8782bd30087bcf08f
Lin Chen Xiang <clin0026@student.monash.edu>
2021-09-29 16:39:50 +0800

    Added plans for this unittest

--- test/solar_energy_duration_unittest.py
+++ test/solar_energy_duration_unittest.py
@@ -3,6 +3,6 @@ import unittest
 from datetime import date, time
 
 class TestSolarEnergyDuration(unittest.TestCase):
-
+    # t1- error , t2-start>sunset, t3-normal
     def setUp(self) -> None:
         self.calculator = Calculator()
