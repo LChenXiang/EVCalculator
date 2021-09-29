75c8bc9c9e51df3d74bdd24fddfd56766e1a5b4b
Lin Chen Xiang <clin0026@student.monash.edu>
2021-09-29 16:18:22 +0800

    Added validation for get_solar_energy_duration to ensure start_time is before end_time

--- app/calculator.py
+++ app/calculator.py
@@ -161,9 +161,17 @@ class Calculator():
     # get the sunrise and sunset, min max the start and the end, then just get the
     # difference and convert it to hours or something.
     def get_solar_energy_duration(self, start_time: time, end_time: time, input_date: date, postcode: str):
+
+        # t1- error , t2-start>sunset, t3-normal
+
         # THIS FUNCTION SHOULD ONLY BE USED FOR REQ 2!
         # Tell me if u want me to change this to better suit need (NYK)
+
+        if start_time > end_time:
+            raise ValueError
+
         sunrise_sunset = self.get_sunrise_sunset(input_date, postcode)
+
         start_time_actual = max(start_time, sunrise_sunset[0])
         end_time_actual = min(end_time, sunrise_sunset[1])
         start_time_delta = timedelta(hours=int(start_time_actual.hour),
@@ -412,3 +420,4 @@ class Calculator():
             current_date_time = new_datetime
 
         return round(cost, 2)
+
